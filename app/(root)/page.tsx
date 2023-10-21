54af5a2094ddc23741963ba8e187373c9fdde34e
atharv-av <atharvv333@gmail.com>
2023-10-22 01:16:23 +0530

    shadcn ui setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+      <div>
+        <Button size="default">
+            Click Me
+        </Button>
+      </div>
+    )
+  }
+  
